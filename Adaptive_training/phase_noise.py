4d76f42db230de86ee18132708f9424fee35aaf9
Maimaiti Nazhamaiti <55472294+mamat-thu@users.noreply.github.com>
2023-07-21 19:31:12 +0800

    Delete phase_noise.py

--- Adaptive_training/phase_noise.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import scipy.io as sio
-MASK_PIXEL_NUM = 600 * 600
-phase_noise = np.random.normal(0, 0.26 * np.pi, (1, MASK_PIXEL_NUM))
-sio.savemat('./onn_output/mask_phase_noise_0.26pi.mat', {'phase_noise':phase_noise})
-
